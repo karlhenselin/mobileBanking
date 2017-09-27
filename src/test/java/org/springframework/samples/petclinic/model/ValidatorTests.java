c085930de4f05912e43a2e17423603bc1ad5b66b
Eugene Fedorenko <fedor@MacBook-Pro.flexagon>
2017-09-27 14:17:30 -0500

    outstanding

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -12,6 +12,9 @@ import org.junit.Test;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+
+
+
 /**
  * @author Michael Isvy
  *         Simple test to make sure that Bean Validation is working
